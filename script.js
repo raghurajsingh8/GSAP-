877cf58659118790d97df878396887abaa7a0ca9
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-05-05 11:47:35 +0530

    gsap box animation intro to .to .from

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to("#box1" , {
+x:1000,
+duration:2,
+delay:1,
+rotate:360,
+backgroundColor:"blue",
+borderRadius:"50%",
+scale:0.5,
+})
+gsap.to("#box2" , {
+    y:1000,
+    duration:2,
+    delay:1
+    ,
+
+    x:1000
+    })
