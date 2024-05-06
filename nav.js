0ee1435fdf3e9da71c1e8bf810db92a30998816d
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-05-06 20:50:42 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,21 @@
+var t1=gsap.timeline()
+
+t1.from("h2",{
+    y:-30,
+    opacity:0,
+})
+t1.from("h4",{
+    y:-30,
+    opacity:0,
+    duration:1,
+    stagger:0.3
+})
+t1.from("h1",{
+
+    opacity:0,
+    y:20,
+    Scale:0.2,
+    duration:0.5
+
+
+})
