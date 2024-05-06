50f49cdb47d7281f3ddc1d797719c4343fd0bc54
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-05-06 07:47:44 +0530

    text_animation and stagger

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-// box_animation
-gsap.to("#box1" , {
-x:1000,
-duration:2,
-delay:1,
-rotate:360,
-backgroundColor:"blue",
-borderRadius:"50%",
-scale:0.5,
-})
-gsap.to("#box2" , {
-    y:1000,
-    duration:2,
-    delay:1
-    ,
+// gsap.to("#box1" , {
+// x:1000,
+// duration:2,
+// delay:1,
+// rotate:360,
+// backgroundColor:"blue",
+// borderRadius:"50%",
+// scale:0.5,
+// })
+// gsap.to("#box2" , {
+//     y:1000,
+//     duration:2,
+//     delay:1
+//     ,
 
-    x:1000
-    })
+//     x:1000
+//     })
+
+gsap.from("h1",{
+opacity:0,
+    duration:2,
+    delay:1,
+    x:20,
+    y:120,
+    stagger:0.5,
+    // +1 dene pr upper se aayega
+    // -1 dene pr niche se aayega (content wise)
+
+})
