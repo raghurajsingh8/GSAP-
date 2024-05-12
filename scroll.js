59f64e41448c3e05074cd591a79c9a619fbd4270
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-05-13 02:00:16 +0530

    Add files via upload
    scrollTrigger:kha se targetr kre scoll ko
    Scrub :-scroll area se perfomance dena

--- scroll.js
+++ scroll.js
@@ -1,36 +1,86 @@
-// delay ya time line use krne pr user ke scroll liye bina hi animation ho jayega isliye user ke samne anime ho isliye scroll use kreneg
-// cdn <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js" integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-gsap.from("#page1 #box",{
-    scale:0,
-    delay:1,
-    duration:2,
-    rotation:360,
+// // delay ya time line use krne pr user ke scroll liye bina hi animation ho jayega isliye user ke samne anime ho isliye scroll use kreneg
+//  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js" integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+// gsap.from("#page1 #box",{
+//     // scale:0,
+//     delay:1,
+//     duration:2,
+//     rotation:360,
 
 
-}) 
+// }) 
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     // delay:1,
+//     duration:2,
+//     // rotation:360,
+//     scrollTrigger:"#page2 #box"
+    
+
+// }) 
+
+// gsap.from("#page3 #box",{
+//     scale:0,
+//     // delay:1,
+//     duration:2,
+//     rotation:360,
+//     scrollTrigger:{
+//         trigger:"#page3 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+
+
+//     }
+
+// }) 
+
+// gsap.from("#page2 h1 ",{
+//     opacity:0,
+//     x:150,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+
+
+// })
+// gsap.from("#page2 h2 ",{
+//     opacity:0,
+//     x:-150,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+
+
+
+
 
 gsap.from("#page2 #box",{
     scale:0,
-    // delay:1,
-    duration:2,
+    opacity:0,
+    duration:11,
     rotation:360,
-    scrollTrigger:"#page2 #box"
-    
-
-}) 
-
-gsap.from("#page3 #box",{
-    scale:0,
-    // delay:1,
-    duration:2,
-    rotation:360,
-    scrollTrigger:{
-        trigger:"#page3 #box",
+    borderRadius:"50%",
+    scrollTrigger: {
+        trigger:"#page2 #box",
         scroller:"body",
         markers:true,
-        start:"top 60%"
+        start:"top 60%",
+        end:"top 30%",
+        scrub:1,
+    },
+})
 
 
-    }
-
-}) 
+// why my scrub and trigger not work
+// it may be because of wrong syntax or wrong value of start and end prop. 
+// also make sure to include the correct path of the element in the trigger prop. 
+// and also make sure to include the correct value of scroller prop. 
